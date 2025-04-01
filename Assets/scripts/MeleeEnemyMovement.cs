4687467359be4eb0ee1407a880b78fa7fa453318
Javier <padrongarciajavier04@gmail.com>
2025-04-01 17:22:53 +0100

    scripts de movimiento

---
+++ Assets/scripts/MeleeEnemyMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = player.position;
+        agent.SetDestination(targetPosition);
+    }
+}
